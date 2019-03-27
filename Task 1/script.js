6fc6562889cddc3f3bed8e3a5d30c1162fe04e02
Artiom Pavliukovich <47572556+apavliukovich@users.noreply.github.com>
2019-03-27 22:12:23 +0300

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,38 +1,63 @@
 var inputX = document.getElementsByTagName('input')[0];
 var inputY = document.getElementsByTagName('input')[1];
 var inputButton = document.getElementsByTagName('input')[2];
-var valueX, valueY;
 
-inputX.onkeyup = function () {
-    valueX = +inputX.value;
-    if (typeof valueX === 'number' && 0 < valueX && valueX < 10 && !!valueX) {
-        inputButton.removeAttribute('disabled');
-    } else {
-        inputButton.setAttribute('disabled', 'disabled');
-        alert('Введено некорректное значение!');
-    }
-};
-inputY.onkeyup = function () {
-    valueY = +inputY.value;
-    if (typeof valueY === 'number' && 0 < valueY && valueY < 10 && !!valueY) {
-        inputButton.removeAttribute('disabled');
-    } else {
-        inputButton.setAttribute('disabled', 'disabled');
-        alert('Введено некорректное значение!');
-    }
-};
+
+inputX.onkeyup = btnDisabled;
+inputY.onkeyup = btnDisabled;
+
+function btnDisabled() {
+    inputButton.disabled = !(inputX.value.trim() && inputY.value.trim());
+}
 
 inputButton.onclick = function () {
+
+    var valueX = +inputX.value,
+        valueY = +inputY.value;
+
+    if (!(valueX && Number.isInteger(valueX) && valueX >= 1 && valueX <= 10)) {
+        resetForm([inputX]);
+        showError(inputX);
+    }
+
+    if (!(valueY && Number.isInteger(valueY) && valueY >= 1 && valueY <= 10)) {
+        resetForm([inputY]);
+        showError(inputY);
+    }
+
+    if (
+        (valueX && Number.isInteger(valueX) && valueX >= 1 && valueX <= 10) &&
+        (valueY && Number.isInteger(valueY) && valueY >= 1 && valueY <= 10)
+    ) {
+        resetForm([inputX, inputY]);
+        drawChessBoard(valueX,valueY)
+    }
+
+
+    function showError(input) {
+        alert('Введите корректное значение в поле ' + input.id.toUpperCase() + ' - целое число от 1 до 10.');
+    }
+
+    function resetForm(inputs) {
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+
+        btnDisabled();
+    }
+};
+
+function drawChessBoard(valueX,valueY) {
     var exist = document.getElementById('table');
     if (exist) exist.parentNode.removeChild(exist);
 
     var table = document.createElement("table");
     table.setAttribute('id', 'table');
 
-    for (var i = 0; i < valueX; i++) {
+    for (var i = 0; i < valueY; i++) {
         var tr = document.createElement('tr');
 
-        for (var j = 0; j < valueY; j++) {
+        for (var j = 0; j < valueX; j++) {
             var td = document.createElement('td');
             if (i % 2 === j % 2) {
                 td.className = "white";
@@ -44,8 +69,9 @@ inputButton.onclick = function () {
         table.appendChild(tr);
     }
     document.body.appendChild(table);
-    table.addEventListener('click', changeColor, false);
+
 };
+window.addEventListener('click', changeColor, false);
 
 function changeColor(evt) {
     var target = evt.target;
